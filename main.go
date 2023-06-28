0c5d42d4474cfcd589201b0afa19f538f1d954ad
JP-GECT <100013491+JP-GECT@users.noreply.github.com>
2023-06-28 16:41:40 +0530

    second

--- main.go
+++ main.go
@@ -6,3 +6,9 @@ type book struct {
 	Author   string `json:"author"`
 	Quantity int    `json:"quantity"`
 }
+
+var books = []book{
+	{ID: "1", Title: "in search of lost time", Author: "marcel proust", Quantity: 2},
+	{ID: "2", Title: "in search of lost time", Author: "marcel proust", Quantity: 5},
+	{ID: "3", Title: "in search of lost time", Author: "marcel proust", Quantity: 6},
+}
