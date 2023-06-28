868c1cf6d691bca0a3184e73ecb9b8a7a85b0a4f
JP-GECT <100013491+JP-GECT@users.noreply.github.com>
2023-06-28 16:54:28 +0530

    added getbooks function

--- main.go
+++ main.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
 type book struct {
 	ID       string `json:"id"`
 	Title    string `json:"title"`
@@ -11,4 +17,16 @@ var books = []book{
 	{ID: "1", Title: "in search of lost time", Author: "marcel proust", Quantity: 2},
 	{ID: "2", Title: "in search of lost time", Author: "marcel proust", Quantity: 5},
 	{ID: "3", Title: "in search of lost time", Author: "marcel proust", Quantity: 6},
+}
+
+func getBooks(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, books)
+
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/books", getBooks)
+	router.Run("localhost:8080")
+
 }
